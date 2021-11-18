c8d6ca9f53612a13dfbb957f0547d8553922c391
Alina_Did_It <ilaila.singer@gmail.com>
2021-11-18 19:26:34 +0200

    Delete 6.py

--- 6.py
+++
@@ -1,5 +0,0 @@
-# Write a script to check whether a specified
-# value is contained in a group of values.
-item = input ('Enter your item: ')
-lst = input ('Enter your list of items: ')
-print(item in lst)
