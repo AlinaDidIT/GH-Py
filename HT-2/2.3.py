bb9da080b6ecbb3e8293c13deb532a0f1b115673
Alina_Did_It <ilaila.singer@gmail.com>
2021-11-22 15:34:04 +0200

    Update 2.3.py

--- HT-2/2.3.py
+++ HT-2/2.3.py
@@ -1,5 +1,5 @@
 # Написати скрипт, який видалить пусті елементи із списка. Список можна "захардкодити".
 
 lst = [(), (), ('',), ('a', 'b'), {}, ('a', 'b', 'c'), ('d'), '', ['',]]
-lst[:] = [item for item in lst if item and item != ('',) and item != ['',]]
-print(lst)
+lst2 = [item for item in lst if item]
+print(lst2)
