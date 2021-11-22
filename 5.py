b6ce86fd160a4a7186d11b4edbe192a7f3069042
Alina_Did_It <ilaila.singer@gmail.com>
2021-11-22 13:40:56 +0200

    Delete 5.py

--- 5.py
+++
@@ -1,5 +0,0 @@
-# Write a script to convert decimal to hexadecimal
-decimal = int(input('Enter the number: '))
-hexadecimal = hex(decimal)
-noprefix_hex = hexadecimal.replace('0x', '')
-print(noprefix_hex)
