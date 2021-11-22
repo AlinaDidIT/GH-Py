037934d70da98e6b7b68c2b3ef59ed6c6e4b21b1
Alina_Did_It <ilaila.singer@gmail.com>
2021-11-22 13:40:28 +0200

    Delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-# Write a script which accepts a sequence
-# of comma-separated numbers from user and
-# generate a list and a tuple with those numbers
-numbers = input('Enter your numbers here: ')
-lst = numbers.split(",")
-tpl = tuple(lst)
-print(lst)
-print(tpl)
