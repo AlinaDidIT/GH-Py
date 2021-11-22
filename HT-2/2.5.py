e96aa7eb88b00894780bd82604d1fbdea17ec4f9
Alina_Did_It <ilaila.singer@gmail.com>
2021-11-22 15:36:48 +0200

    Update 2.5.py

--- HT-2/2.5.py
+++ HT-2/2.5.py
@@ -3,11 +3,11 @@
 # Словник для роботи захардкодити свій.
 
 people = {'Andrew': 39, 'Ben': 25, 'Rick': 48, 'David': 39}
-values = set()
+values = list()
 new_people = {}
 
 for k, v in people.items():
     if v not in values:
         new_people.update({k: v})
-        values.add(v)
-print(new_people)
+        values.append(v)
+print(new_people)eople)
