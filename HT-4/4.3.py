3899c0c8a45a20e0dfffd87684429add58ec91b9
Alina_Did_It <ilaila.singer@gmail.com>
2021-11-29 19:28:11 +0200

    Update 4.3.py

--- HT-4/4.3.py
+++ HT-4/4.3.py
@@ -3,15 +3,15 @@
 # и False - якщо ні
 
 def is_prime(number):
+    k = 0
     for i in range(2, number // 2+1):
         if (number % i == 0):
-            k = k+1
+          k = k+1
     if (k <= 0):
         print("True")
     else:
         print("False")
 
-k = 0
 number = int(input('Enter the number: '))
 
 is_prime(number)
