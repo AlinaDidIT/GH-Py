47a56aff8a8ff55c4dbfa5f26cb96360ede0424a
Alina_Did_It <ilaila.singer@gmail.com>
2021-11-29 16:53:27 +0200

    Add files via upload

---
+++ HT-4/4.4.py
@@ -0,0 +1,19 @@
+# 4. Написати функцію < prime_list >, яка прийматиме 2 аргументи - 
+# початок і кінець діапазона, і вертатиме список простих чисел
+# всередині цього діапазона.
+
+def prime_list(start, finish):
+    for i in range(start, finish+1):
+        k = 0
+        for j in range(1,i+1):
+            if i%j == 0:
+                k += 1
+        if k == 2:
+            lst.append(i)
+    print(lst)
+
+
+start = int(input('Enter the begining of your range: '))
+finish = int(input('Enter the ending of your range: '))
+lst = []
+prime_list(start, finish)
