93d9a3136806134f17ef0798275aa069c11a4c24
Alina_Did_It <ilaila.singer@gmail.com>
2021-12-02 16:29:13 +0200

    Update 5.3.py

--- HT-5/5.3.py
+++ HT-5/5.3.py
@@ -7,23 +7,20 @@
 
 class LoginException(Exception):
     pass
-    
 
-def login(users):
-    status = ''
-    try:
-        for user in users:
-            if len(user[0])>3 and len(user[0])<50 and len(user[1]) > 8:
-                for i in user:
-                    for s in i[0]:
-                        if s.isupper() == True:
-                            if any(l.isdigit() for l in user[1]) == True:
-                                status = 'Ok'
-                                print('Username:', user[0], '\nPassword:', user[1], '\nStatus:', status)
-                                
-    except LoginException:
-        status = 'False'
-        print('Username:', user[0], '\nPassword:', user[1], '\nStatus:', status)
+def login(username, password):
+    
+        if len(username)<3 or len(username)>50:
+            raise LoginException("The username must be more than 3 symbols and less than 50")
+        elif username[0].isupper() != True:
+            raise LoginException("The first letter of the username must be upper case")
+        elif len(password) < 8:
+            raise LoginException("The password must be more than 8 symbols")
+        elif any(s.isdigit() for s in password) != True:
+            raise LoginException("The password must include at least one digit")
+        else:
+            print('Your login parameters are correct')
 
 users = [['Chucha', 'dfjkghlskdjhg'], ['df', '45687532'], ['Dima', 'sdfgs54sdfg'], ['sunRise', 'sdjkjhjh98']]
-login(users)
+for user in users:
+    login(user[0], users[1])
