3dd7e2354a75e76c8c0a016f4f59543065afce0a
Alina_Did_It <ilaila.singer@gmail.com>
2021-12-02 16:59:09 +0200

    Update 5.3.py

--- HT-5/5.3.py
+++ HT-5/5.3.py
@@ -9,7 +9,7 @@ class LoginException(Exception):
     pass
 
 def login(username, password):
-    
+
         if len(username)<3 or len(username)>50:
             raise LoginException("The username must be more than 3 symbols and less than 50")
         elif username[0].isupper() != True:
@@ -17,10 +17,10 @@ def login(username, password):
         elif len(password) < 8:
             raise LoginException("The password must be more than 8 symbols")
         elif any(s.isdigit() for s in password) != True:
-            raise LoginException("The password must include at least one digit")
+            raise LoginException("The password must include at least one digit")    
         else:
-            print('Your login parameters are correct')
+            print('Ok')
 
 users = [['Chucha', 'dfjkghlskdjhg'], ['df', '45687532'], ['Dima', 'sdfgs54sdfg'], ['sunRise', 'sdjkjhjh98']]
 for user in users:
-    login(user[0], users[1])
+    print('Name:', user[0], '\nPassword:', user[1], '\nStatus:', login(user[0], users[1]))
