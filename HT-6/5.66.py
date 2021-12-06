3237e2e3b0c67ccc88f3438a821271a5b6c278bc
Alina_Did_It <ilaila.singer@gmail.com>
2021-12-06 12:13:42 +0200

    Update 5.66.py

--- HT-6/5.66.py
+++ HT-6/5.66.py
@@ -5,11 +5,24 @@
 # можна почитати документацію по ній:
 # https://docs.python.org/3/library/stdtypes.html#range
 
-def gen(user_input):
-    for i in range(1, iter_obj+1, 5):
-            yield i/100*20
+def generator(stop, start=None, step=1):
+    if step == 0:
+        step = 1
+    elif start != None:
+        stop,start = start,stop
+    else:
+        start = 0
+    try:
+        if step > 0:
+            while start < stop:
+                yield start
+                start += step
+        if step < 0:
+            while start > stop:
+                yield start
+                start += step
+    except ValueError:
+        pass
 
-
-user_input = int(input('Enter your number: '))
-for i in gen(user_input):
+for i in generator(10, 1, -2):
     print(i)
