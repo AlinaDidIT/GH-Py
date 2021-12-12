ae30e8178b7a93a4597bdac01d052b44d6427899
Alina_Did_It <ilaila.singer@gmail.com>
2021-12-12 23:12:23 +0200

    Update 7.1.py

--- HT-8/7.1.py
+++ HT-8/7.1.py
@@ -1,6 +1,26 @@
-import json
-import pprint
-
+counter = 0
+while counter < 3:      
+    username = input('Please, enter your username: ').rstrip()
+    password = input('Enter your password (4 digits): ')
+    counter += 1
+    if counter == 3:
+        break
+    else:
+        if len(username)<3 or len(username)>25:
+            print('The username must be more than 3 and less than 25 symbols')
+            counter += 1
+        elif username[0].isupper() != True:
+            print('The first letter of the username must be upper case')
+            counter += 1
+        elif len(password) != 4:
+            print('The password must consist of 4 numbers')
+            counter += 1
+        elif password.isdigit() != True:
+            print('The password must include only digits')
+            counter += 1 
+        else:
+            break
+            
 class LoginException(Exception):
     pass
 
@@ -14,36 +34,23 @@ def login_menu():
                 }
     return login_menu
 
-def menu1():
+def menu1():    
 
-    def validator(username, password):
-
-          if len(username)<3 or len(username)>25:
-              raise LoginException("The username must be more than 3 and less than 25 symbols")
-          elif username[0].isupper() != True:
-              raise LoginException("The first letter of the username must be upper case")
-          elif len(str(password)) != 4:
-              raise LoginException("The password must consist of 4 numbers")
-          elif type(password) != int:
-              raise LoginException("The password must include only digits")
-          else:
-              return '\n\nValidation is successful'
-
-    def balance_file(username):
+    def balance_file():
 
         balance = {'balance:': 0}
         with open(username + '_balance.data.json', 'w') as balance_f:
             json.dump(balance, balance_f, indent=2, ensure_ascii=False)
         return balance
 
-    def transactions_file(username):
+    def transactions_file():
 
         transaction = {username: 0}
         with open(username + '_transactions.json', 'w') as transactions_f:
             json.dump(transaction, transactions_f, indent=2, ensure_ascii=False)
         return transaction
 
-    def registration(username, password):
+    def registration():
 
         try:
             with open('users.data.json') as file:
@@ -54,31 +61,33 @@ def menu1():
         user = {username: password}
         user_i = user.items()
         users_data_i = users_data.items()
-        for k,v in user_i:
-            if user_i & users_data_i == user_i:
-                print('\n\nYour account already exists.')
-            else:
-                users_data.update(user)
-                validator(username, password)
-                with open('users.data.json', 'w') as file:
-                    json.dump(users_data, file, indent=2, ensure_ascii=False)
-                print('\n\nCongratulations! You\'ve got your personal card :)')
+        if user_i & users_data_i != user_i:
+            users_data.update(user)
+            with open('users.data.json', 'w') as file:
+                json.dump(users_data, file, indent=2, ensure_ascii=False)
+                balance_file()
+                transactions_file()
+                print('Congratulations! You\'ve got your personal card :)')
+        else:
+            print('Your account already exists.')
+
         return users_data
 
-    def authorization(username, password):
+    def authorization():
 
-        print('\n\nHello!')
+        print('Hello!')
         user = {username: password}
         with open('users.data.json') as file:
             users_data = json.load(file)
-            user_i = user.items()
-            users_data_i = users_data.items()
-            for k,v in user_i:
-                if user_i & users_data_i == user_i:
-                    print('\n\nPlease, choose the operation: ')
-                else:
-                    print('Sorry, there is no such client.\n\nTo SignUp, please, enter 'r' button or try again: ')
-                    # показать меню авторизации
+        user_i = user.items()
+        users_data_i = users_data.items()
+
+        for k,v in user_i:
+            if user_i & users_data_i == user_i:
+                print('Please, choose the operation: ')
+            else:
+                print('Sorry, there is no such client.To SignUp, please, enter 'r' button or try again: ')
+
         return user
 
     menu1_switch = {
@@ -87,16 +96,11 @@ def menu1():
                     }
 
     menu1 = login_menu()
-    print('Welcome! Choose the SignUP or LogIn operation.\n\n(To continue press any other button) : ')
+    print('Welcome! Choose the SignUP or LogIn operation.(To continue press any other button) : ')
     for key in menu1:
         print(key, '->', menu1[key])
     choice1 = int(input('Enter the number of your choice: '))
     output1 = menu1_switch.get(choice1)()
-    if choice1 == 2:
-        choice = 1
-        output1 = menu1_switch.get(choice1)()
-    else:
-        pass
 
 def operations_menu():
 
@@ -104,7 +108,7 @@ def operations_menu():
                 'Cash payment': 1,
                 'Withdraw money': 2,
                 'View balance': 3,
-                'View transaction history': 2,
+                'View transaction history': 4,
                 }
     return operations_menu
 
@@ -117,15 +121,15 @@ def menu2():
             for k,balance in balance_data.items():
                 print('Your current', k, balance)
 
-    def view_transaction_history(transactions_file_json):
+    def view_transaction_history():
 
-        with open(username +'_transactions.data.json', 'r') as transactions_f:
+        with open(username +'_transactions.json', 'r') as transactions_f:
             json_data = json.loads(transactions_f)
             for k, transaction in json_data.items():
                 print(k, transaction)
         return json_data
 
-    def cash_payment(username):
+    def cash_payment():
 
         cash_payment = int(input('Enter the amount of cash you want to pay: '))
         if type(cash_payment) == int:
@@ -145,7 +149,7 @@ def menu2():
             raise NumberException("Not number was entered. Please, try again.")
         return balance_data
 
-    def withdraw_money(username):
+    def withdraw_money():
 
         input_withdraw = int(input('Enter the amount of cash you want to withdraw: '))
         with open(username +'_balance.data.json', 'r') as balance_f:
@@ -170,10 +174,10 @@ def menu2():
         return balance_data
 
     menu2_switch = {
-                    1: view_balance,
-                    2: view_transaction_history,
                     1: cash_payment,
-                    2: withdraw_money
+                    2: withdraw_money,
+                    3: view_balance,
+                    4: view_transaction_history
                     }
 
     menu2 = operations_menu()
@@ -206,8 +210,18 @@ def menu2():
     # # режим як "інкассація", за допомогою якого в нього можна
     # # "загрузити" деяку кількість банкнот (вибирається номінал і кількість)
 
-menu1()
-menu2()
+def currency():
 
-username = input('Please, enter your username: ')
-password = int(input('Enter your password (4 digits): '))
+    currency = {
+                 1000: 1000, 
+                 500: 1000,
+                 200: 1000,
+                 100: 1000,
+                 50: 1000,
+                 20: 1000,
+                 10: 1000
+                 }
+    currency = json.dumps(currency, indent=2)
+    currency_in_stock = json.loads(currency)
+    return currency, currency_in_stock
+
